5181ba24de710cdcf8e0465c09d0621990206439
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-07-09 06:22:57 -0700

    setup utils for dates config, shadcn, globals

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-      <h1 className='text-3xl underline'>Home</h1>
+      <h1 className='text-3xl underline text-white'>Home</h1>
+      <Button>Click me come on</Button>
     </div>
   );
 }
---
+++ components/theme-provider.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+
+export function ThemeProvider({ children, ...props}: ThemeProviderProps) {
+    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
